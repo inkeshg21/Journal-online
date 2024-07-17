6bda156dbcec0ef3d8ff23eabaf8b4a07f115587
inkeshg21 <inkeshg@gmail.com>
2024-07-17 21:07:05 +0545

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { ContextProvider } from "./contexts/ContextProvider";
+import App from "./App";
+import "./index.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ContextProvider>
+      <App />
+    </ContextProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
